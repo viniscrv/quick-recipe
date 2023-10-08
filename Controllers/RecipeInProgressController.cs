d6d2f128d5027ac0b18c9569d381a270d73a7710
Vinícius <viniscrv@gmail.com>
2023-10-07 21:16:27 -0300

    feat: adicionado controllers para manipular receita em andamento

--- Controllers/RecipeInProgressController.cs
+++ Controllers/RecipeInProgressController.cs
@@ -63,11 +63,129 @@ public class RecipeInProgressController : ControllerBase
 
         var recipe = await _context.Recipes.Include(r => r.Processes)
             .FirstOrDefaultAsync(r => r.Id == user.RecipeInProgress.RecipeId);
-        
+
         if (recipe == null) return NotFound(new { errorMessage = "Recipe not founded" });
 
-        var currentProcess = recipe.Processes.FirstOrDefault(p => p.Order == user.RecipeInProgress.CurrentStep);
+        var currentProcess = recipe.Processes
+            .Where(p => p.Order == user.RecipeInProgress.CurrentStep)
+            .Select(p => new
+            {
+                p.Name,
+                p.Details,
+                p.Order,
+                p.TimeInSeconds,
+            });
 
         return Ok(currentProcess);
+    }
+
+    [HttpGet("next-step")]
+    [Authorize]
+    public async Task<IActionResult> nextStep()
+    {
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+        var user = await _context.Users.Include(u => u.RecipeInProgress).FirstOrDefaultAsync(u => u.Email == userEmail);
+
+        if (user == null) return NotFound(new { errorMessage = "User not founded" });
+        if (user.RecipeInProgress == null)
+            return BadRequest(new { errorMessage = "User does not have a recipe in progress" });
+
+        var recipe = await _context.Recipes.Include(r => r.Processes)
+            .FirstOrDefaultAsync(r => r.Id == user.RecipeInProgress.RecipeId);
+
+        if (recipe == null) return NotFound(new { errorMessage = "Recipe not founded" });
+
+        var quantityProcesses = recipe.Processes.Count();
+
+        user.RecipeInProgress.CurrentStep += 1;
+
+        if (user.RecipeInProgress.CurrentStep > quantityProcesses)
+        {
+            // maybe use this in future
+            user.RecipeInProgress.Finished = true;
+
+            _context.RecipeInProgresses.Update(user.RecipeInProgress);
+            _context.RecipeInProgresses.Remove(user.RecipeInProgress);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "All steps have been completed, bon appetit" });
+        }
+
+        _context.RecipeInProgresses.Update(user.RecipeInProgress);
+        await _context.SaveChangesAsync();
+        
+        var currentProcess = recipe.Processes
+            .Where(p => p.Order == user.RecipeInProgress.CurrentStep)
+            .Select(p => new
+            {
+                p.Name,
+                p.Details,
+                p.Order,
+                p.TimeInSeconds,
+            });
+
+        return Ok(currentProcess);
+    }
+    
+    [HttpGet("back-step")]
+    [Authorize]
+    public async Task<IActionResult> backStep()
+    {
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+        var user = await _context.Users.Include(u => u.RecipeInProgress).FirstOrDefaultAsync(u => u.Email == userEmail);
+
+        if (user == null) return NotFound(new { errorMessage = "User not founded" });
+        if (user.RecipeInProgress == null)
+            return BadRequest(new { errorMessage = "User does not have a recipe in progress" });
+
+        var recipe = await _context.Recipes.Include(r => r.Processes)
+            .FirstOrDefaultAsync(r => r.Id == user.RecipeInProgress.RecipeId);
+
+        if (recipe == null) return NotFound(new { errorMessage = "Recipe not founded" });
+
+        if (user.RecipeInProgress.CurrentStep == 1) return BadRequest(new { errorMessage = "You are already in the first step" });
+
+        user.RecipeInProgress.CurrentStep -= 1;
+        
+        _context.RecipeInProgresses.Update(user.RecipeInProgress);
+        await _context.SaveChangesAsync();
+        
+        var currentProcess = recipe.Processes
+            .Where(p => p.Order == user.RecipeInProgress.CurrentStep)
+            .Select(p => new
+            {
+                p.Name,
+                p.Details,
+                p.Order,
+                p.TimeInSeconds,
+            });
+
+        return Ok(currentProcess);
+    }
+    
+    [HttpPost("finish-recipe")]
+    [Authorize]
+    public async Task<IActionResult> finishRecipe()
+    {
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+        var user = await _context.Users.Include(u => u.RecipeInProgress).FirstOrDefaultAsync(u => u.Email == userEmail);
+
+        if (user == null) return NotFound(new { errorMessage = "User not founded" });
+        if (user.RecipeInProgress == null)
+            return BadRequest(new { errorMessage = "User does not have a recipe in progress" });
+
+        var recipe = await _context.Recipes.Include(r => r.Processes)
+            .FirstOrDefaultAsync(r => r.Id == user.RecipeInProgress.RecipeId);
+
+        if (recipe == null) return NotFound(new { errorMessage = "Recipe not founded" });
+
+        // maybe use this in future
+        user.RecipeInProgress.Finished = true;
+
+        _context.RecipeInProgresses.Update(user.RecipeInProgress);
+        _context.RecipeInProgresses.Remove(user.RecipeInProgress);
+        await _context.SaveChangesAsync();
+
+        return Ok();
     }
 }
